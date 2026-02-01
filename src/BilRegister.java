66b6ca19b6a6d0983e5734782f20571ea5f65e31
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2026-02-01 15:59:06 +0100

    Metoder klar

--- src/BilRegister.java
+++ src/BilRegister.java
@@ -7,4 +7,40 @@ public class BilRegister {
     public BilRegister() {
         this.bilar = new ArrayList<Bil>();
     }
+
+    public Bil hittaBil(String registreringsnummer){
+        for (Bil bil: bilar){
+            if (bil.getRegistreringsnummer().equals(registreringsnummer)) {
+                return bil;
+            }
+        }
+        return null;
+    }
+
+    // Lägg till bil
+    public void läggTillBil(Bil bil){
+        if (hittaBil(bil.getRegistreringsnummer()) != null) {
+            throw new IllegalArgumentException("En bil detta registrerings nummer finns redan");
+        }
+        bilar.add(bil);
+    }
+
+    // Ta bort bil
+    public void taBortBil(String registreingsnummer){
+        Bil bil = hittaBil(registreingsnummer);
+        if (bil == null) {
+            throw new IllegalArgumentException("Ingen bil med registreringsnumret finns");
+        }
+        bilar.remove(bil);
+    }
+
+    // String metod
+    public String toString(){
+        String resultat = "";
+
+        for(Bil bil: bilar){
+            resultat+=bil + "\n";
+        }
+        return resultat;
+    }
 }
